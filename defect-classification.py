5101c38e6fd60645636cdbaa8104938517038524
Jakub <kubus.pospolitos@gmail.com>
2024-11-27 14:11:59 +0100

    Gotowe

--- defect-classification.py
+++ defect-classification.py
@@ -1,7 +1,18 @@
+import joblib
 import streamlit as st
 
 
 st.title("Defect Classification")
+
+
+text_defektu = st.text_area("Wpisz opis defektu")
+
+wyslij_do_modelu = st.button("Klasyfikuj")
+
+if wyslij_do_modelu:
+    model = joblib.load("defect_classifier.pkl")
+    prediction = model.predict([text_defektu])
+    st.text(f"Kategoria defektu: {prediction[0]}")
 
 
 
